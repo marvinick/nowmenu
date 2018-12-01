75a097cf23fc50fe3272f3b22ba18da03fde3941
Marvin Nikijuluw <marvkipi@gmail.com>
2018-11-30 21:11:08 -0800

    passing properties review

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,8 +1,9 @@
 class ReviewsController < ApplicationController 
     before_action :set_item
+    before_action :set_review, only: [:show, :edit, :destroy, :update]
 
     def new 
-        @review = Review.new(project_id: params[:project_id])
+        @review = Review.new(project_id: params[:project_id], item_id: params[:item_id])
     end 
 
     def create 
@@ -16,6 +17,20 @@ class ReviewsController < ApplicationController
 
     def show; end 
 
+    def edit; end 
+
+    def update 
+        if @review.update_attributes(review_params)
+            redirect_to project_item_review_path(@project, @item, @review)
+        else
+            render :edit 
+        end 
+    end 
+
+    def destroy
+    end 
+
+
     private 
 
     def review_params 
@@ -24,12 +39,18 @@ class ReviewsController < ApplicationController
         #     whitelisted[:properties] = params[:review][:properties]
         #     whitelisted[:public_review] = params[:review][:public_review]
         #     whitelisted[:private_review] = params[:review][:private_review]
+        #     whitelisted[:project_id] = params[:review][:project_id]
+        #     whitelisted[:item_id] = params[:review][:item_id]
         # end
     end 
 
     def set_item 
         @project = Project.find(params[:project_id])
         @item = @project.items.find(params[:item_id])
+    end 
+
+    def set_review 
+        @review = @item.reviews.find(params[:id])
     end 
 
 end 
