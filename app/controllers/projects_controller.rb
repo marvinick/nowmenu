6ff807d92b7b910a8c99b20023aa214682fdae4e
Marvin Nikijuluw <marvkipi@gmail.com>
2018-12-08 22:46:22 -0800

    css

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,54 +1,55 @@
-class ProjectsController < BaseController 
-    
+class ProjectsController < BaseController
+
     before_action :set_project, only: [:show, :edit, :update, :destroy]
 
-    def index 
-        @projects = current_user.projects 
-    end 
+    def index
+        @projects = current_user.projects
+    end
 
-    def new 
-        @project = Project.new 
-    end 
+    def new
+        @project = Project.new
+    end
 
     def create
         @project = Project.new(project_params)
+        @project.user_id = current_user
         @project.project_users.new(user: current_user, role: "owner" )
-        if @project.save 
+        if @project.save
             redirect_to project_path(@project)
-        else 
+        else
             render :new
-        end 
-    end 
+        end
+    end
 
     def show
         @project = Project.find(params[:id])
-    end 
+    end
 
-    def edit; end 
+    def edit; end
 
-    def update 
+    def update
         if @project.update_attributes(project_params)
             redirect_to project_path(@project)
-        else 
+        else
             render :edit
-        end 
-    end 
+        end
+    end
 
     def destroy
         if @project.destroy
-            redirect_to root_path 
-        else 
+            redirect_to root_path
+        else
             redirect_to project_path(@project)
-        end 
-    end 
+        end
+    end
 
-    private 
+    private
 
     def project_params
-        params.require(:project).permit(:name, :description, categories_attributes: [:name, :_destroy, :required, :id])
-    end 
+        params.require(:project).permit(:name, :description, :user_id, categories_attributes: [:name, :_destroy, :required, :id])
+    end
 
-    def set_project 
+    def set_project
         @project = current_user.projects.find(params[:id])
-    end 
-end 
+    end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181201231158) do
+ActiveRecord::Schema.define(version: 20181209030031) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 20181201231158) do
     t.datetime "updated_at", null: false
     t.string "name"
     t.text "description"
+    t.integer "user_id"
   end
 
   create_table "reviews", force: :cascade do |t|
