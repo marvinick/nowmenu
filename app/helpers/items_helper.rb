4bebffbbae9ca7df23b8fc0a47ac7e7c0172aae1
Marvin Nikijuluw <marvkipi@gmail.com>
2018-12-13 20:05:57 -0800

    calculation wrong

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -1,5 +1,18 @@
-module ItemsHelper 
+module ItemsHelper
   def each_item_review_average
+    float = sum_review_average / @item.reviews.count
+    float.floor
+  end
 
+  def sum_review_average
+    total_value = []
+    sum_of_each_review_average = []
+    @item.reviews.each do |review|
+      review.properties.each do |k, v|
+        total_value << v.to_i
+      end
+      sum_of_each_review_average << total_value.sum / review.properties.count
+    end
+    sum_of_each_review_average.sum
   end
 end
