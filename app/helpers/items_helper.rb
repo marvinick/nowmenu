e46c318e3fc62b2f81be9280eb019fc49246bd72
Marvin Nikijuluw <marvkipi@gmail.com>
2018-12-14 18:21:52 -0800

    starting basecamp rejected redesign design

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -5,14 +5,14 @@ module ItemsHelper
   end
 
   def sum_review_average
+    total = []
     total_value = []
     sum_of_each_review_average = []
     @item.reviews.each do |review|
       review.properties.each do |k, v|
         total_value << v.to_i
       end
-      sum_of_each_review_average << total_value.sum / review.properties.count
+      return total_value.sum / review.properties.count
     end
-    sum_of_each_review_average.sum
   end
 end
