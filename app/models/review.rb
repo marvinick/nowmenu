44eb30960075dbf7e5cd3b1192292f566d79b054
Marvin Nikijuluw <marvkipi@gmail.com>
2019-01-02 12:27:59 -0800

    view

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,7 @@
 class Review < ApplicationRecord
     belongs_to :project
     belongs_to :item
+    belongs_to :user
     serialize :properties, Hash
 
     validates_presence_of :public_review, length: {minimum: 5, maximum: 500}, allow_blank: false
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,12 @@
 class User < ApplicationRecord
-  has_many :project_users 
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+
+  devise :invitable, :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
+
+  has_many :project_users
   has_many :projects, through: :project_users
 
   attribute :email, :string
-  
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :invitable, :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
 
-  
 end
