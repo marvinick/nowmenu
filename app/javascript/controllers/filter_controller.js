a9c27ed53c04cca9de5ec8d65157da8f883f573a
Marvin Nikijuluw <marvkipi@gmail.com>
2019-03-06 22:00:24 -0600

    filter list

---
+++ app/javascript/controllers/filter_controller.js
@@ -0,0 +1,16 @@
+import { Controller } from 'stimulus'
+
+export default class extends Controller {
+
+  static targets = [ "source", "filterable" ]
+
+  filter(event) {
+    let lowerCaseFilterTerm = this.sourceTarget.value.toLowerCase()
+
+    this.filterableTargets.forEach((el, i) => {
+      let filterableKey = el.getAttribute("data-filter-key")
+
+      el.classList.toggle("filter--notFound", !filterableKey.includes(lowerCaseFilterTerm) )
+    })
+  }
+}
