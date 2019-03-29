b34e92c112e3128958506fb4b5875181aebe70be
Marvin Nikijuluw <marvkipi@gmail.com>
2019-03-29 00:22:15 -0500

    view update

--- app/models/group.rb
+++ app/models/group.rb
@@ -5,6 +5,10 @@ class Group < ApplicationRecord
   tracked owner: Proc.new { |controller, model| controller.current_user ? controller.current_user : nil }
 
   belongs_to :project
+
+  validates_presence_of :name, length: {minimum: 3, maximum: 30}, allow_blank: false
+  validates_presence_of :description, length: {minimum: 5, maximum: 200}, allow_blank: false
+
   has_many :group_items
   has_many :items, through: :group_items
 end
