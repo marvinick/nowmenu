ed471f637328d59a8ceb863647206443f27d4d48
Marvin Nikijuluw <marvkipi@gmail.com>
2019-04-19 00:12:05 -0500

    custom validation error in locale

--- app/models/category.rb
+++ app/models/category.rb
@@ -7,5 +7,5 @@ class Category < ApplicationRecord
   belongs_to :project
 
   validates_presence_of :name, length: {minimum: 5, maximum: 20}, allow_blank: false
-  validates :description, length: {minimum: 5, maximum: 100}, allow_blank: false
+  validates_presence_of :description, length: {minimum: 10, maximum: 100}, allow_blank: false
 end
