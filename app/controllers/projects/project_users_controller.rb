0ac8fe23f348337b91c29174b8d45c64461a51fb
Marvin Nikijuluw <marvkipi@gmail.com>
2019-05-09 22:06:33 -0500

    update views

--- app/controllers/projects/project_users_controller.rb
+++ app/controllers/projects/project_users_controller.rb
@@ -1,33 +1,37 @@
 class Projects::ProjectUsersController < BaseController
-    before_action :set_project
+  before_action :set_project
 
-    def index
-      @project_users = @project.project_users.all
+  def index
+    @project_users = @project.project_users.all
+  end
+
+  def create
+    project_user = @project.project_users.new(project_user_params)
+    project_user.project = @project
+
+    if project_user.save
+      ProjectUserMailer.invite(project_user).deliver_now
+      redirect_to @project, notice: 'Saved!'
+    else
+      redirect_to @project, alert: 'Failed saving!'
     end
+  end
 
-    def create
-      project_user = @project.project_users.new(project_user_params)
-      project_user.project = @project
+  def show
+    @user = User.find(params[:id])
+  end
 
-      if project_user.save
-        ProjectUserMailer.invite(project_user).deliver_now
-        redirect_to @project, notice: 'Saved!'
-      else
-        redirect_to @project, alert: 'Failed saving!'
-      end
-    end
+  def destroy
 
-    def destroy
-      
-    end
+  end
 
-    private
+  private
 
-    def set_project
-        @project = current_user.projects.find(params[:project_id])
-    end
+  def set_project
+    @project = current_user.projects.find(params[:project_id])
+  end
 
-    def project_user_params
-        params.require(:project_user).permit(:email)
-      end
+  def project_user_params
+    params.require(:project_user).permit(:email)
+  end
 end
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,72 +1,72 @@
 class ReviewsController < BaseController
-    before_action :set_item
-    before_action :set_review, only: [:show, :edit, :destroy, :update, :review_chart]
-    caches_action :index, :show, :new, :edit
+  before_action :set_item
+  before_action :set_review, only: [:show, :edit, :destroy, :update, :review_chart]
+  caches_action :index, :show, :new, :edit
 
-    def new
-      @review = Review.new(project_id: params[:project_id], item_id: params[:item_id])
+  def new
+    @review = Review.new(project_id: params[:project_id], item_id: params[:item_id])
+  end
+
+  def create
+    @review = Review.new(review_params)
+    @review.user_id = current_user.id
+    if @review.save
+        redirect_to [@project, @item]
+    else
+        render :new
     end
+  end
 
-    def create
-      @review = Review.new(review_params)
-      @review.user_id = current_user.id
-      if @review.save
-          redirect_to [@project, @item]
-      else
-          render :new
-      end
+  def show
+    @review_score = Daru::Vector.new(each_review_average)
+  end
+
+  def edit; end
+
+  def index
+    @reviews = @item.reviews.includes(:user)
+  end
+
+  def update
+    if @review.update_attributes(review_params)
+      redirect_to project_item_review_path(@project, @item, @review)
+    else
+      render :edit
     end
+  end
 
-    def show
-      @review_score = Daru::Vector.new(each_review_average)
+  def destroy
+    if @review.destroy
+      redirect_to [@project, @item], alert: 'deleted!'
     end
+  end
 
-    def edit; end
+  def review_chart
+    @reviews_chart = render json: @review.properties
+  end
 
-    def index
-      @reviews = @item.reviews.includes(:user)
+
+  private
+
+  def review_params
+    params.require(:review).permit(:project_id, :item_id, :user_id, :private_review, :public_review, properties: {})
+  end
+
+  def set_item
+    @project = Project.find(params[:project_id])
+    @item = @project.items.find(params[:item_id])
+  end
+
+  def set_review
+    @review = @item.reviews.find(params[:id])
+  end
+
+  def each_review_average
+    review_total_value = []
+    @review.properties.each do |k, v|
+      review_total_value << v.to_i
     end
-
-    def update
-      if @review.update_attributes(review_params)
-        redirect_to project_item_review_path(@project, @item, @review)
-      else
-        render :edit
-      end
-    end
-
-    def destroy
-      if @review.destroy
-        redirect_to [@project, @item], alert: 'deleted!'
-      end
-    end
-
-    def review_chart
-      @reviews_chart = render json: @review.properties
-    end
-
-
-    private
-
-    def review_params
-      params.require(:review).permit(:project_id, :item_id, :user_id, :private_review, :public_review, properties: {})
-    end
-
-    def set_item
-      @project = Project.find(params[:project_id])
-      @item = @project.items.find(params[:item_id])
-    end
-
-    def set_review
-      @review = @item.reviews.find(params[:id])
-    end
-
-    def each_review_average
-      review_total_value = []
-      @review.properties.each do |k, v|
-        review_total_value << v.to_i
-      end
-      review_total_value
-    end
+    review_total_value
+  end
 
 end
